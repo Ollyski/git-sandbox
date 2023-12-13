3ce71afccd19126a2aea6e7b2de21455dc3a67e7
Ollyski <146211302+Ollyski@users.noreply.github.com>
2023-12-13 12:01:29 -0500

    Add files via upload

---
+++ silinski-asgn07-names/functions/utility-functions.php
@@ -0,0 +1,12 @@
+
+
+@param Mixed
+
+@return Nothing
+
+function dd($value) {
+  echo "<pre>";
+  var_dump($value);
+  echo "</pre>";
+  exit();
+}
---
+++ silinski-asgn07-names/index.php
@@ -0,0 +1,52 @@
+<?php
+  include 'functions/utility-functions.php';
+  include 'functions/names-functions.php';
+
+  $fileName = 'names-short.txt';
+  $fullNames = load_full_names($fileName);
+
+$firstNames = load_first_names($fullNames);
+
+$lastNames = load_last_names($fullNames);
+
+
+
+  for($i = 0; $i < sizeof ($fullNames); $i++) {
+    if (ctype_alpha($lastNames[$i].$firstNames[$i])) {
+      $validFirstNames[$i] = $firstNames[$i];
+      $validLastNames[$i] = $lastNames[$i];
+      $validFullNames[$i] = $validLastNames[$i] . "," . $validFirstNames[$i];
+    }
+  }
+
+  //results//
+
+  echo '<h1>Names - Results </h1>';
+
+  echo '<h2>All Names</h2>';
+
+  echo '<ul style="list-style-type:none">';
+    foreach($fullNames as $fullName) {
+      echo "<li>$fullName</li>";
+    }
+
+  echo "</ul>";
+
+  echo '<h2> All Valid Names</h2>';
+  echo "<p> There are " . sizeof($validFullNames) . " valid names</p>";
+  echo '<ul style="list-style-type:none">';
+    foreach($validFullNames as $validFullName) {
+      echo "<li>$validFullName</li>";
+    }
+echo "</ul>";
+
+echo '<h2>Unique Names</h2>';
+$uniqueValidNames = (array_unique($validFullNames));
+echo (<p>There are " . sizeof($uniqueValidNames") . " Unique names</p>";
+echo '<ul style="list-style-type:none">';
+  foreach($uniqueValidNames as $uniqueValidNames) {
+    echo "<li>$uniqueValidNames</li>";
+  }
+
+ 
+?>
